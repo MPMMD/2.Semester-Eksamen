963dcb90040024b93fb4e9a9b34dac6dc70b7d96
Martin P <mart803d@edu.zealand.dk>
2022-05-31 21:23:38 +0200

    OmOs opsætning og start på burgermenu

---
+++ main.js
@@ -0,0 +1,9 @@
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_mobile_navbar
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
