3396ef2e4390880bdeb4dc45377995b70163828a
Martin P <mart803d@edu.zealand.dk>
2022-06-01 15:51:47 +0200

    NexaBlack + Vi er Hopscreek placeholder side almost done

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_mobile_navbar
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+ function burgerFunction() {
+     var x = document.querySelector('#dropdown');
+     if (x.style.display === "block") {
+       x.style.display = "none";
+     } else {
+       x.style.display = "block";
+     }
+   }
+
